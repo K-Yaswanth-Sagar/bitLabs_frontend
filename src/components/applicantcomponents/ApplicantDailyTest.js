d1b0cea7e34446968a0c78000f109e03864a27a9
K-Yaswanth-Sagar <yaswanthsagar0307@gmail.com>
2025-04-09 21:25:14 +0530

    added daily-test component

---
+++ src/components/applicantcomponents/ApplicantDailyTest.js
@@ -0,0 +1,9 @@
+function ApplicantDailyTest(){
+    return(
+
+    <h1 style={{textAlign: 'center',padding:'100px' }}>
+    Daily tests comming soon.....</h1>
+)
+}
+
+export default ApplicantDailyTest;
